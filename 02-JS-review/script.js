c94a8155e361be5b0e8ae29afda6a4efcab55dd3
David Meskhishvili <dmeskh@gmail.com>
2025-01-18 20:02:43 -0700

    Array filter method

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -205,5 +205,12 @@ const essentialData = books.map((book) => ({
 essentialData;
 
 // Array filter() method
-const longBooks = books.filter((book) => book.pages > 500);
-longBooks;
+const longBooksWithMovie = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+longBooksWithMovie;
+
+const adventureBooks = books
+  .filter((book) => book.genres.includes("adventure"))
+  .map((book) => book.title);
+adventureBooks;
